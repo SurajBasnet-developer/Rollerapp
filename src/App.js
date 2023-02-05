1611c5242b8fd8d9274d673db444c152f87c63dc
Suraj <surajbasnet449@gmail.com>
2023-02-05 12:22:18 +0545

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [diceNum, setDiceNum] = useState(3);
+  const reDice = () => {
+    const Randno = Math.floor(Math.random() * 6) + 1;
+    setDiceNum(Randno);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <img
+          width={300}
+          height={300}
+          src={require(`./assets/${diceNum}.png`)}
+        ></img>
+        <br></br>
+        <button onClick={() => reDice()} className="button">
+          Roll
+        </button>
+      </center>
     </div>
   );
-}
+};
 
 export default App;
